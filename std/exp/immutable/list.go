c33936262b8291c72d65f9aea366d3ece8242ae4
Emanuel Bennici <eb@fabmation.de>
2024-08-21 11:38:06 +0200

    Correct generic definition of immutable.List

--- std/exp/immutable/list.go
+++ std/exp/immutable/list.go
@@ -4,34 +4,34 @@ import "go.l0nax.org/typact"
 
 // FromSlice returns a new [List] from s.
 // After this function has been called, s should not be used anymore.
-func FromSlice[S ~[]E, E any](s S) List[S, E] {
-	return List[S, E]{
+func FromSlice[T any](s []T) List[T] {
+	return List[T]{
 		value: s,
 	}
 }
 
 // List is an immutable list of elements.
-type List[S ~[]E, E any] struct {
-	value S
+type List[T any] struct {
+	value []T
 }
 
 // Get returns the value at the given index.
 // If the index is below zero or greater than len(list) - 1, [typact.None] will be returned.
-func (l List[S, E]) Get(index int) typact.Option[E] {
+func (l List[T]) Get(index int) typact.Option[T] {
 	if index < 0 || index >= len(l.value) {
-		return typact.None[E]()
+		return typact.None[T]()
 	}
 
 	return typact.Some(l.value[index])
 }
 
 // Len returns the number of elements in the list.
-func (l List[S, E]) Len() int {
+func (l List[T]) Len() int {
 	return len(l.value)
 }
 
 // Iter is an iterator over the elements in the list.
-func (l List[S, E]) Iter(yield func(index int, value E) bool) {
+func (l List[T]) Iter(yield func(index int, value T) bool) {
 	for i, v := range l.value {
 		if !yield(i, v) {
 			break
